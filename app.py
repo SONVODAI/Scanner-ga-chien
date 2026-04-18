3e1a34ec07ce9309786331ca6bb56693b29387a5
SONVODAI <sonv92769@gmail.com>
2026-04-18 19:51:56 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
 import streamlit as st
 
-st.title("Scanner Gà Chiến - OK")
+st.set_page_config(page_title="Scanner Gà Chiến", layout="centered")
+
+st.title("Scanner Gà Chiến")
+st.success("App chạy OK")
+st.write("Nếu anh thấy dòng này thì hệ thống đã ổn.")
